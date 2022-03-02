af3d56fe046f813ec8de4364a8b1276f30c5107b
akshitgupta65 <akshitgupta65@gmail.com>
2022-03-02 12:37:51 +0530

    html,css improved; JS tasks listed

---
+++ script.js
@@ -0,0 +1,10 @@
+// TASKS
+// 1. (window) Load each section after 25% scroll
+
+// 2. (who-am-I) Insert a video message, also make some space for it so that it doesnot ruin the layout of the page after video has been loaded
+
+// 3. (my-work) when hover on window, slide the project description from above and blacken the background image
+
+// 4. (my-work) add all projects in a row (100,200,300,400,...)
+
+// 5. (my-work) to view my projects create a slider, when user clicks right button, slide the slider to left and vice versa
